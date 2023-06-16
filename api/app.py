ea541db7e4af5c9fd27238a0ab0b84d45a611544
Msmohits <88817177+Msmohits@users.noreply.github.com>
2023-06-16 20:13:50 +0530

    task_done

--- api/app.py
+++ api/app.py
@@ -8,32 +8,17 @@ import uuid
 
 app = FastAPI()
 
-
 response_data = {}
-
-async def receive():
-    connection = await connect(os.getenv('test'))
-    channel = await connection.channel()
-    queue1 = await channel.declare_queue("hell2", durable=True)
-    # # await queue1.purge()
-    async with queue1.iterator() as queue_iter:
-        async for message in queue_iter:
-            async with message.process():
-                response_data.update(json.loads(message.body))
-                # print('RRRRRRRRR', response_data)
-
-                return response_data
-
 
 @app.post('/send')
 async def send(request: dict):
+    print(os.getenv('test'))
     connection = await connect(os.getenv('test'))
     message_id = uuid.uuid4()
     print(message_id)
     request['message_id'] = str(message_id)
 
     async with connection:
-
         channel = await connection.channel()
         queue = await channel.declare_queue("hello")
         message = json.dumps(request).encode()
@@ -43,16 +28,15 @@ async def send(request: dict):
         )
 
         print("Sent:", request)
-
-        try:
-            for i in range(10):
-                response = await receive()
-                if str(message_id) == str(response['message_id']):
-                    return response
-        except TimeoutError:
-            print("Response not found, timeout")
-
+        queue1 = await channel.declare_queue("hell2", durable=True)
+        async with queue1.iterator() as queue_iter:
+            async for message in queue_iter:
+                async with message.process():
+                    message = json.loads(message.body)
+                    response_data.update(message)
+                    if str(message_id) == str(message.get('message_id')):
+                        return response_data
 
 if __name__ == "__main__":
-    # asyncio.run(receive())
-    uvicorn.run("app:app", host=os.getenv('test'), port=8001)
+    loop = asyncio.get_event_loop()
+    uvicorn.run("app:app", host=os.getenv('test'), port=6001)
